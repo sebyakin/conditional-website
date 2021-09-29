b970ec8a705d64bfcc1a87eb12f885ca680ce2b8
sebyakin <46461635+sebyakin@users.noreply.github.com>
2021-09-30 01:07:41 +0300

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Block from "./components/Block/Index";
+import "./app.css"
 
 function App() {
+
+  const windowOuterWidth = window.outerWidth;
+  const windowOuterHeight = window.outerHeight;
+  const screenAvWidth = window.screen.availWidth;
+  const screenAvHeight = window.screen.availHeight;
+  const deviceMemory = navigator.deviceMemory;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='data'>
+         <p>*Screens*</p>
+        <p>Outer width is {windowOuterWidth},</p> 
+        <p>Outer height is {windowOuterHeight},</p> 
+        <p>Available Width Is {screenAvWidth}, </p>
+        <p>Available Height Is {screenAvHeight}, </p>
+        <p>*Device*</p>
+        <p>Device Memory Is {deviceMemory}, </p>
+      </div>
+
+      <div className='container'> 
+        <Block text={windowOuterWidth / 7}/>
+        <Block text={windowOuterHeight}/>
+        <Block text={screenAvWidth * 0.03}/>
+
+      </div>
+
     </div>
+
+
   );
 }
 
---
+++ src/components/Block/Index.jsx
@@ -0,0 +1,8 @@
+const Block = ({text}) => {
+
+return (
+    <div style={{borderRadius: text, background: "blue", width: "100vw", height: "100%"}} ></div>
+    )
+}
+
+export default Block
